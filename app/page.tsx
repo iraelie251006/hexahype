cfa3458216a627b286c4c1882a55cb09d878298a
Elie <niyubwayoiraelie5777@gmail.com>
2025-06-19 17:20:40 +0200

    Added Beam background

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-
+import Beams from "@/components/Beams";
 
 export default function Home() {
   return (
-    <h1 className="text-4xl flex justify-center">Hhhh we are Hexahype</h1>
+<div className="w-full h-screen relative">
+  <Beams
+    beamWidth={4.4}
+    beamHeight={25}
+    beamNumber={44}
+    lightColor="#14DBD8"
+    speed={10}
+    noiseIntensity={0}
+    scale={0.2}
+    rotation={30}
+  />
+</div>
   );
 }
